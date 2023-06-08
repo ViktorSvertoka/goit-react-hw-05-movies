06d0f2f8fd93f2ab7345fb58a4d46ecde9ec2eea
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-08 23:46:46 +0300

    Feat(Files) Added new files

---
+++ src/services/TmbdApi.jsx
@@ -0,0 +1,8 @@
+// Импортируем модуль axios для работы с HTTP-запросами
+import axios from 'axios';
+
+// Устанавливаем базовый URL для всех запросов
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+// Константа с API-ключом
+const API_KEY = '992758a4802a699e8df27d4d6efc34fb';
