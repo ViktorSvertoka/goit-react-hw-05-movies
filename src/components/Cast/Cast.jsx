a52510272eb7ef5598b17817acae9ec311b22e02
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-10 23:59:27 +0300

    Test

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,26 +1,27 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchActors } from '..//../services/TmbdApi';
-// import Loader from '../Loader/Loader';
+import Loader from '../Loader/Loader';
 
 const Cast = () => {
   const { movieId } = useParams(); // Получение параметра movieId из URL
   const [actors, setActors] = useState([]); // Состояние для хранения списка актеров
-  // const [loading, setLoading] = useState(false); // Состояние для отображения состояния загрузки
+  const [loading, setLoading] = useState(false); // Состояние для отображения состояния загрузки
 
   useEffect(() => {
     const onActorsOfMovie = () => {
-      // setLoading(true); // Устанавливаем флаг загрузки в true
+      setLoading(true); // Устанавливаем флаг загрузки в true
 
-      fetchActors(movieId).then(actors => {
-        setActors(actors); // Обновляем состояние актеров с полученными данными
-      });
-      // .catch(error => {
-      //   console.log(error); // Обрабатываем ошибку, если произошла
-      // })
-      // .finally(() => {
-      //   setLoading(false); // Устанавливаем флаг загрузки в false после выполнения запроса
-      // });
+      fetchActors(movieId)
+        .then(actors => {
+          setActors(actors); // Обновляем состояние актеров с полученными данными
+        })
+        .catch(error => {
+          console.log(error); // Обрабатываем ошибку, если произошла
+        })
+        .finally(() => {
+          setLoading(false); // Устанавливаем флаг загрузки в false после выполнения запроса
+        });
     };
 
     onActorsOfMovie(); // Вызываем функцию для получения актеров фильма
@@ -28,31 +29,26 @@ const Cast = () => {
 
   return (
     <div>
-      {/* {loading && <Loader />} */}
+      {loading && <Loader />}
 
       <ul>
-        {actors.length > 0
-          ? actors.map(
-              ({ id, profile_path, original_name, name, character }) => (
-                <li key={id}>
-                  <img
-                    width="200px"
-                    src={
-                      profile_path
-                        ? `https://image.tmdb.org/t/p/w500${profile_path}`
-                        : `https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg`
-                    }
-                    alt={original_name}
-                  />
-                  <p>{name}</p>
-                  <p>Character: {character}</p>
-                </li>
-              )
-            )
-          : "Sorry, there isn't any info"}
+        {actors.map(({ id, profile_path, original_name, name, character }) => (
+          <li key={id}>
+            <img
+              width="200px"
+              src={
+                profile_path
+                  ? `https://image.tmdb.org/t/p/w500${profile_path}`
+                  : `https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg`
+              }
+              alt={original_name}
+            />
+            <p>{name}</p>
+            <p>Character: {character}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
-
 export default Cast;
