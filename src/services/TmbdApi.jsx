82d1045ba08a42b8edea9ec2a41d97d275a1fbf8
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-10 22:53:24 +0300

    Update TmbdApi.jsx

--- src/services/TmbdApi.jsx
+++ src/services/TmbdApi.jsx
@@ -1,11 +1,11 @@
 // Импортируем модуль axios для работы с HTTP-запросами
 import axios from 'axios';
 
-// Константа с API-ключом
-const API_KEY = '992758a4802a699e8df27d4d6efc34fb';
-
 // Устанавливаем базовый URL для всех запросов
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+// Константа с API-ключом
+const API_KEY = '992758a4802a699e8df27d4d6efc34fb';
 
 // Функция для получения трендовых фильмов
 export const fetchTrending = async () => {
