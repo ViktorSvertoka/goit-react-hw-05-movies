f8a4633567ede323c92ebfdc26aab3fefe95cb93
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-11 16:44:03 +0300

    Styles(JSX) Added styles to MovieDetails

--- src/pages/MovieDetails/MovieDetails.styled.jsx
+++ src/pages/MovieDetails/MovieDetails.styled.jsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   display: flex;
@@ -12,4 +13,26 @@ export const List = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
+  list-style-type: none;
+`;
+
+export const ListInfo = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`;
+
+export const LinkInfo = styled(Link)`
+  text-decoration: none;
+  font-weight: 600;
+  font-size: 18px;
+  color: #191d1e;
+
+  &:hover {
+    color: tomato;
+  }
 `;
