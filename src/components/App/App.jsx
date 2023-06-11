db5395db3eadd90e86bcb6e16b77f184a51d537b
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-11 10:19:41 +0300

    Test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,35 @@
-import { lazy } from 'react';
+// import { lazy } from 'react';
+
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from 'components/Layout/Layout';
 
+import Cast from 'components/Cast/Cast';
+import Reviews from 'components/Reviews/Reviews';
+import HomePage from 'pages/Home/Home';
+import MovieDetailsPage from 'pages/MovieDetails/MovieDetails';
+import MoviesPage from 'pages/Movies/Movies';
+
 // Ленивая загрузка компонентов
-const Home = lazy(() => import('pages/Home/Home'));
-const Movies = lazy(() => import('pages/Movies/Movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
-const Cast = lazy(() => import('components/Cast/Cast'));
-const Reviews = lazy(() => import('components/Reviews/Reviews'));
+// const Home = lazy(() => import('pages/Home/Home'));
+// const Movies = lazy(() => import('pages/Movies/Movies'));
+// const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+// const Cast = lazy(() => import('components/Cast/Cast'));
+// const Reviews = lazy(() => import('components/Reviews/Reviews'));
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route index element={<HomePage />} />
 
-        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies" element={<MoviesPage />} />
 
-        <Route path="/movies/:movieId" element={<MovieDetails />}>
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
           <Route path="/movies/:movieId/cast" element={<Cast />} />
           <Route path="/movies/:movieId/reviews" element={<Reviews />} />
         </Route>
 
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
   );
