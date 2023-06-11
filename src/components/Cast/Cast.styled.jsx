a6d2771304a213dcae0bce6bdecbfd0a8eb3a3e2
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-06-11 16:26:04 +0300

    Styles(JSX) Added styles to Cast & Reviews

---
+++ src/components/Cast/Cast.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+  list-style-type: none;
+`;
+
+export const Text = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { fetchReviews } from 'services/TmbdApi';
 import Loader from 'components/Loader/Loader';
+import { List } from './Reviews.styled';
 
 const Reviews = () => {
   const { movieId } = useParams();
@@ -32,14 +33,14 @@ const Reviews = () => {
       {loading && <Loader />}
       {reviews.length !== 0 && (
         <div>
-          <ul>
+          <List>
             {reviews.map(review => (
               <li key={review.id}>
                 <h2>Author: {review.author}</h2>
                 <p>{review.content}</p>
               </li>
             ))}
-          </ul>
+          </List>
         </div>
       )}
       {reviews.length === 0 && (
